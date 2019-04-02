84ebe66dc7cf414308715bc1929051cdf06f0f77
Ngai To <ngai_to_lo@hotmail.com>
2019-04-01 21:50:28 -0700

    adding more

--- econ_proj/js/d4.js
+++ econ_proj/js/d4.js
@@ -1,7 +1,79 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
+
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+width = 600 - margin.left - margin.right,
+height = 300 - margin.top - margin.bottom;
+
+// Parse the date / time
+var	parseDate = d3.time.format("%Y-%m").parse;
+
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+var xAxis1 = d3.svg.axis()
+.scale(x)
+.orient("bottom")
+.tickFormat(d3.time.format("%Y-%m"));
+
+var yAxis1 = d3.svg.axis()
+.scale(y)
+.orient("left")
+.ticks(10);
+
+
+var svg1 = d3.select("#chart").append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", 
+      "translate(" + margin.left + "," + margin.top + ")");
+// load data
+d3.csv("./data/bar-data.csv", function(error, data) {
+
+data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.value = +d.value;
+    console.log(d.value);
+});
+
+x.domain(data.map(function(d) { return d.date; }));
+y.domain([0, d3.max(data, function(d) { return d.value; })]);
+// y.domain(data.map(function(d) { return d.value; }));
+svg1.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis1)
+.selectAll("text")
+  .style("text-anchor", "end")
+  .attr("dx", "-.8em")
+  .attr("dy", "-.55em")
+  .attr("transform", "rotate(-90)" );
+
+svg1.append("g")
+  .attr("class", "y axis")
+  .call(yAxis1)
+.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .text("Value ($)");
+
+svg1.selectAll("bar")
+  .data(data)
+.enter().append("rect")
+  .style("fill", "steelblue")
+  .attr("x", function(d) { return x(d.date); })
+  .attr("width", x.rangeBand())
+  .attr("y", function(d) { return y(d.value); })
+  .attr("height", function(d) { return height - y(d.value); });
+
+});
+
+var margin1 = {top: 20, right: 20, bottom: 30, left: 40},
     padding = {top: 0, right: 0, bottom: 30, left: 50},
-    width = 860 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width1 = 860 - margin1.left - margin1.right,
+    height1 = 500 - margin1.top - margin1.bottom;
 
 /* 
  * value accessor - returns the value to encode for a given data object.
@@ -12,13 +84,13 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
 
 // setup x 
 var xValue = function(d) { return d.Calories;}, // data -> value
-    xScale = d3.scale.linear().range([0, width]), // value -> display
+    xScale = d3.scale.linear().range([0, width1]), // value -> display
     xMap = function(d) { return xScale(xValue(d));}, // data -> display
     xAxis = d3.svg.axis().scale(xScale).orient("bottom");
 
 // setup y
 var yValue = function(d) { return d["Protein (g)"];}, // data -> value
-    yScale = d3.scale.linear().range([height, 0]), // value -> display
+    yScale = d3.scale.linear().range([height1, 0]), // value -> display
     yMap = function(d) { return yScale(yValue(d));}, // data -> display
     yAxis = d3.svg.axis().scale(yScale).orient("left");
 
@@ -27,11 +99,11 @@ var cValue = function(d) { return d.Manufacturer;},
     color = d3.scale.category10();
 
 // add the graph canvas to the body of the webpage
-var svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+var svg = d3.select("#chart2").append("svg")
+    .attr("width", width1 + margin1.left + margin1.right)
+    .attr("height", height1 + margin1.top + margin1.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin1.left + "," + margin1.top + ")");
 
 // add the tooltip area to the webpage
 var tooltip = d3.select("body").append("div")
@@ -55,11 +127,11 @@ d3.csv("./data/cereal.csv", function(error, data) {
   // x-axis
   svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", "translate(0," + height1 + ")")
       .call(xAxis)
     .append("text")
       .attr("class", "label")
-      .attr("x", width)
+      .attr("x", width1)
       .attr("y", -6)
       .style("text-anchor", "end")
       .text("Calories");
@@ -109,14 +181,14 @@ d3.csv("./data/cereal.csv", function(error, data) {
 
   // draw legend colored rectangles
   legend.append("rect")
-      .attr("x", width - 18)
+      .attr("x", width1 - 18)
       .attr("width", 18)
       .attr("height", 18)
       .style("fill", color);
 
   // draw legend text
   legend.append("text")
-      .attr("x", width - 24)
+      .attr("x", width1 - 24)
       .attr("y", 9)
       .attr("dy", ".35em")
       .style("text-anchor", "end")
